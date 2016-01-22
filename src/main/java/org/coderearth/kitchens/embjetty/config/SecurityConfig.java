a2c9aaff587adc39666d17571aaf77568e05f15c
Kunal Patel <kunal_patel@symantec.com>
2016-01-22 20:11:31 +0530

    make spring security in place with embedded jetty

--- src/main/java/org/coderearth/kitchens/embjetty/config/SecurityConfig.java
+++ src/main/java/org/coderearth/kitchens/embjetty/config/SecurityConfig.java
@@ -2,17 +2,35 @@ package org.coderearth.kitchens.embjetty.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * Created by kunal_patel on 20/01/16.
  */
 @Configuration
 @EnableWebSecurity
-@ImportResource({"classpath:webSecurityConfig.xml"})
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	public SecurityConfig() {
-		super();
+		System.out.println("Registering SpringSecurityContext !!");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER").and()
+				.withUser("admin").password("password").roles("USER", "ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().authorizeRequests().antMatchers("/**").hasAnyRole("USER");
 	}
 }
